89296072fbcc599653e399745302d3d0db54f697
Adam <12666173+phoenixburn119@users.noreply.github.com>
2023-09-25 12:49:22 -0600

    Built Get-Hostname function
    This function will remove the domain at the end of the computer name if it exists and skip if not.

--- Run.ps1
+++ Run.ps1
@@ -1,3 +1,45 @@
-$CVSSource = Import-Csv -Path ""
-$DataSheet = 
+$CVSSource = Import-Csv -Path "Z:\Kinzer_Directory\ADGetDisabledComputers\Source\UnsupportedOSmachines.csv"
+[System.Collections.ArrayList]$DataCollector = @()
 $CSVExport = ".\Export"
+
+Function Get-ADEnabled {
+    [CmdletBinding(DefaultParameterSetName='Hostname')]
+    param(
+        [parameter(Mandatory)]
+        [string] $Number
+    )
+
+
+    $ValueCollector = [pscustomobject]@{'Hostname'=$($CVSSource.Hostname); 'OS Version'=$($CVSSource.OperatingSystem); 'AD Status'=$() }
+
+    $DataCollector.add($ValueCollector)
+}
+Function Get-Hostname {
+    [CmdletBinding(DefaultParameterSetName='CurrentHostname')]
+    param(
+        [parameter(Mandatory)]
+        [string] $CurrentHostname
+    )
+    # Write-Host "$CurrentHostname"
+    If($null -ne $CurrentHostname) {    
+        If((($CurrentHostname).$CurrentHostname.length -8) -ne ".STO.COM") {
+                $HostnameVal = ($CurrentHostname).Substring(0,$CurrentHostname.Length-8)
+                Write-Output "$HostnameVAL"
+            } else {
+                # Write-Output "$CurrentHostname"
+            }
+    } else {
+        
+    }
+}
+# $temp = $("02-08-0913-589L.STO.COM").substring("02-08-0913-589L.STO.COM".length -8)
+
+Get-Hostname -CurrentHostname "TAG1860.STO.COM"
+
+for($idx = 0; $idx -lt 291; $idx++ ) {
+    # Write-Host $CVSSource[$idx].Hostname
+
+    If($null -ne $CVSSource[$idx].Hostname) {
+        Get-Hostname -CurrentHostname $CVSSource[$idx].Hostname
+    }   
+}
